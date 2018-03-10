f4bd01a50e8dfe304be17e0cb257519054ce0a68
ileez <1039025368@qq.com>
2018-03-10 01:51:37 +0000

    Update whistle version

---
+++ kms-raspi3/README.md
@@ -0,0 +1,4 @@
+# KMS version.
+vlmcsd-1111-2017-06-17-Hotbird64
+# Docker Use.
+docker run -d --restart=always --name="kmsrv" -h kms -p 1688:1688 zlee/kms-raspi3:latest
---
+++ kms-raspi3/win10.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo.
+echo 请以右键“以管理员身份运行”。
+
+echo.
+echo 正在激活Windows10。。。
+echo.
+cscript.exe "%SYSTEMDRIVE%\windows\system32\slmgr.vbs" /ipk your system key
+"%SYSTEMDRIVE%\windows\system32\slmgr.vbs" -skms your kms server ip
+"%SYSTEMDRIVE%\windows\system32\slmgr.vbs" /ato
+"%SYSTEMDRIVE%\windows\system32\slmgr.vbs" /dlv
